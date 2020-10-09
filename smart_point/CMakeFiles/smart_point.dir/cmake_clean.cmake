03fd7f1080be0f327fd9f21ce9ae76fe1dd3a32b
yanhuichao <739884701@qq.com>
2020-10-09 17:18:01 +0800

    add smart poionter

---
+++ smart_point/CMakeFiles/smart_point.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smart_point.dir/main.cpp.o"
+  "smart_point.pdb"
+  "smart_point"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/smart_point.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ smart_point/cmake-build-debug/CMakeFiles/smart_point.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smart_point.dir/main.cpp.o"
+  "smart_point"
+  "smart_point.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/smart_point.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
